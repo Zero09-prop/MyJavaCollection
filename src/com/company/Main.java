9013c7ebffea7543e4e689d51978bcf7a4a1fa15
Zero09-prop <72509842+Zero09-prop@users.noreply.github.com>
2021-02-19 16:14:32 +0300

    Add files via upload
    Add method,iterator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,14 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        MyContainer my = new MyContainer();
-        my.Add(6,7);
-        int[] m = new int[2];
-       // ArrayList<Integer> lst = new ArrayList<Integer>();
-       // lst.add(5,5);
-        for(int i = 0; i < 2; i++)
-            System.out.println(m[i]);
+        ArrayList<Integer> mu = new ArrayList<Integer>(4);
+        mu.add(5);
+        mu.add(6);
+        mu.add(6,7);
+        int[] a = new int[2];
+        int[] b;
+        a[0] = 1;
+        a[1] = 3;
+        System.out.println(a);
     }
 }
--- src/com/company/MyContainer.java
+++ src/com/company/MyContainer.java
@@ -1,28 +1,52 @@
 package com.company;
 
-public class MyContainer {
+import java.util.Arrays;
 
-    private int[] objects;
+public class MyContainer
+{
 
+    private int[] _storage;
+    private int _currPos;
     public MyContainer()
     {
-        objects = new int[5];
+        _storage = new int[2];
+        _currPos = 0;
     }
 
     public MyContainer(int capacity)
     {
-        objects = new int[capacity];
+
+        _storage = new int[capacity];
+        _currPos = 0;
     }
 
     public void Add(int index,int element)
     {
-        if(index < objects.length)
+        if(index < _storage.length)
         {
-
+            
         }
         else{
             throw new IndexOutOfBoundsException();
         }
-
     }
+
+    public void Add(int element){
+        if(_currPos < _storage.length)
+        {
+            _storage[_currPos] = element;
+            _currPos++;
+        }
+        else{
+            int[] temp = new int[_storage.length * 2];
+            for(int i = 0; i < _storage.length;i++){
+                temp[i] = _storage[i];
+            }
+            temp[_currPos] = element;
+            _storage = temp;
+            _currPos++;
+        }
+    }
+
+
 }
